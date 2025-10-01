ebb33f131df59768290b28162a146ea6b97b7181
Kin Fortuno <alu0101679112@ull.edu.es>
2025-10-01 16:21:36 +0100

    Añadido compl1

--- 1Cuatri_2º/EstrucComp/Primera Practica ALU/Obj 4/compl1.v
+++ 1Cuatri_2º/EstrucComp/Primera Practica ALU/Obj 4/compl1.v
@@ -1,5 +1,12 @@
 module compl1 (output wire [3:0] Out, input wire [3:0] Inp, input wire cp1);
 
-
+  always @(cp1) begin
+    
+    if (cp1 == 2'b0)
+      Out <= ~Inp;
+    else if (cp1 != 2'b0)
+      Out <= Inp;
+      
+  end
 
 endmodule
