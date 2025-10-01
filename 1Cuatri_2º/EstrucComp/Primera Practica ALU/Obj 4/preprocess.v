5445a85561b61cfdcb4921d068a040188e25e2e0
Kin Fortuno <alu0101679112@ull.edu.es>
2025-10-01 17:55:43 +0100

    Corregido compl1

--- 1Cuatri_2º/EstrucComp/Primera Practica ALU/Obj 4/preprocess.v
+++ 1Cuatri_2º/EstrucComp/Primera Practica ALU/Obj 4/preprocess.v
@@ -1,5 +1,8 @@
 module preprocess(output wire [3:0] AMod, output wire [3:0] BMod, input wire [3:0] A, input wire [3:0] B, input wire [2:0] Op);
 
-  
+  wire add1;
+  wire op1_A;
+  wire op2_B,
+  wire cpl;
 
 endmodule
