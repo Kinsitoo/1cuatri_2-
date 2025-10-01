ebe77f2416731521125b387f1b93034486dec1b6
Kin Fortuno <alu0101679112@ull.edu.es>
2025-10-01 17:44:01 +0100

    Corregido compl1

--- 1Cuatri_2º/EstrucComp/Primera Practica ALU/Obj 4/compl1.v
+++ 1Cuatri_2º/EstrucComp/Primera Practica ALU/Obj 4/compl1.v
@@ -1,12 +1,5 @@
 module compl1 (output wire [3:0] Out, input wire [3:0] Inp, input wire cp1);
 
-  always @(cp1, Inp) begin
-    
-    if (cp1 == 1'b0)
-      Out = ~Inp;
-    else
-      Out = Inp;
-      
-  end
+  assign Out = cp1 ? Inp : ~Inp;
 
 endmodule
