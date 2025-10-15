9ef72417e9772a6e478b95b83801316fb645b18c
Kin Fortuno <alu0101679112@ull.edu.es>
2025-10-15 10:13:11 +0100

    Final P1 EdC

--- 1Cuatri_2º/EstrucComp/Primera_Practica_ALU/P1_final/preprocess.v
+++ 1Cuatri_2º/EstrucComp/Primera_Practica_ALU/P1_final/preprocess.v
@@ -14,9 +14,22 @@ module preprocess(output wire [3:0] AMod, output wire [3:0] BMod, input wire [3:
   end
   */
 
-  mux2_4 mux1(cable1, 4'b0, 4'b1, add1);             // add1
+  mux2_4 mux1(cable1, 4'b0, 4'b1, add1);                   // add1
   mux2_4 mux2(AMod, cable1, A, op1_A);                     // op1_A
   mux2_4 mux3(cable2, A, B, op2_B);                        // op2_B
   compl1 cpl(BMod, cable2, cp1);                           // Cpl
 
 endmodule
+
+
+/*
+
+Operacion   Op2   Op1   Op0   Add1    Op1_A   Op2_B   Cpl
+A+B+c_in    0     0     0     X       1       1       0
+A+1+c_in    0     0     1     1       0       0       0
+-A+c_in     0     1     0     1       0       0       1
+-B+c_in     0     1     1     1       0       1       1
+A and B     1     0     0     X       1       1       0
+a or B      1     0     1     X       1       1       0
+A xor B     1     1     0     X       1       1       0
+Not A       1     1     1     X       X       1       X
