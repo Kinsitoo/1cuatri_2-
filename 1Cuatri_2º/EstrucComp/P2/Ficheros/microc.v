9d512b8035ddc2e7a9d4bf55864db03fdacd65ae
Kin Fortuno <alu0101679112@ull.edu.es>
2025-11-12 20:59:30 +0000

    Actualizado P2 edc

--- 1Cuatri_2º/EstrucComp/P2/Ficheros/microc.v
+++ 1Cuatri_2º/EstrucComp/P2/Ficheros/microc.v
@@ -1,5 +1,33 @@
 module microc(output wire [5:0] Opcode, output wire z, input wire clk, reset, s_inc, s_inm, we3, wez, input wire [2:0] Op);
 //Microcontrolador sin memoria de datos de un solo ciclo
+wire [7:0] rd1, rd2, wd3, alu_out, inm;    
+wire [15:0] instruccion;
+wire [9:0] pc_out, pc_in, dir_salto, mux_out, sum_out;
 
+assign dir_salto = instruccion[9:0];
+
+memprog memoria(instruccion, clk, pc_out);
+
+registro #(10) PC(pc_out, clk, reset, pc_in);
+
+regfile Banco_reg(rd1, rd2, clk, we3, instruccion[3:0], instruccion[7:4], instruccion[11:8], wd3);
+
+alu ALU(alu_out, z, rd1, rd2, Op);
+
+// Mux de la izq del todo
+mux2 #(10) mux1(pc_in, dir_salto, sum_out, s_inc);
+
+// Mux del centro
+assign n1 = 10'b0000000001;
+assign n2 = 10'b0000000010;
+mux2 #(10) mux2(mux_out, n1, n2, s_skip);
+
+// Mux de la derecha
+assign inm = instruccion[11:4];
+mux2 #(10) mux3(wd3, alu_out, inm, s_inm);
+
+sum sumador(sum_out, mux_out, pc_out);
+
+assign OPCODE = instruccion[15:10];
 
 endmodule
--- 1Cuatri_2º/EstrucComp/P2/Ficheros/microc_tb.v
+++ 1Cuatri_2º/EstrucComp/P2/Ficheros/microc_tb.v
@@ -1,29 +1,226 @@
 `timescale 1 ns / 10 ps
 module microc_tb;
 
-// declaración de variables
-//...
+// Declaración de variables
+reg clk, reset, s_inc, s_inm, we3, s_skip;
+reg [2:0] Op;
+wire z;
+wire [5:0] Opcode;
 
-// instanciación del camino de datos
-//...
+// Instanciación del camino de datos
+microc micro(Opcode, z, clk, reset, s_inc, s_inm, we3, s_skip, Op);
 
-// generación de reloj clk
-//...
+// Generación de reloj clk
+always
+begin
+  clk=1;
+  #10;
+  clk=0;
+  #10;
+end
 
 // Reseteo y configuración de salidas del testbench
 initial
 begin
   $dumpfile("microc_tb.vcd");
   $dumpvars;
-  // ... señal de reset
+  
+  //integer idx;
+  //$dumpvars(0,  .br.R[1])
+
+  reset = 1;  
+  #5  
+  reset = 0;
+
 end
 
 // Bloque simulación señales control por ciclo
 initial
 begin
-  // retardos y señales para ejecutar primera instrucción (ciclo 1)
-  // retardos y señales para ejecutar segunda instrucción (ciclo 2)
-  // ...
+  // Ciclo 1 (li 0, R1)
+  #10   
+  s_inc = 1;
+  s_skip = 0;
+  we3 = 1;
+  Op = 3'b000;
+  s_inm = 1;
+
+  // Ciclo 2 (li 1, R2)
+  #20 
+  s_inc = 1;
+  s_skip = 0;
+  we3 = 1;
+  Op = 3'b000;
+  s_inm = 1; 
+
+  // Ciclo 3 (li 4, R3)
+  #20 
+  s_inc = 1;
+  s_skip = 0;
+  we3 = 1;
+  Op = 3'b000;
+  s_inm = 1;
+
+  // Ciclo 4 (li 0, R4)
+  #20 
+  s_inc = 1;
+  s_skip = 0;
+  we3 = 1;
+  Op = 3'b000;
+  s_inm = 1;
+
+  // Ciclo 5 (and R3, R2, R5)(Bucle: )
+  #20
+  s_inc = 1;
+  s_skip = 0;
+  we3 = 1;
+  Op = 3'b100;
+  s_inm = 0; 
+
+  // Ciclo 6 (skipne R5, R4)
+  #20
+  s_inc = 1;
+  s_skip = 0;
+  we3 = 1;
+  Op = 3'b011;
+  s_inm = 0;
+
+  // Ciclo 7 (add R1, R2, R1)
+  #20
+  s_inc = 1;
+  s_skip = 0;
+  we3 = 1;
+  Op = 3'b010;
+  s_inm = 0; 
+
+  // Ciclo 8 (sub R3, R2, R3)
+  #20
+  s_inc = 1;
+  s_skip = 0;
+  we3 = 1;
+  Op = 3'b011;
+  s_inm = 0; 
+
+  // Ciclo 9 (skipeq R3, R2)
+  #20
+  s_inc = 1;
+  s_skip = 0;
+  we3 = 0;
+  Op = 3'b011;
+  s_inm = 0;
+
+  // Ciclo 10 (j Bucle)
+  #20
+  s_inc = 0;
+  s_skip = 0;
+  we3 = 0;
+  Op = 3'b000;
+  s_inm = 0; 
+
+  // Ciclo 11 (and R3, R2, R5)(Bucle: )
+  #20
+  s_inc = 1;
+  s_skip = 0;
+  we3 = 1;
+  Op = 3'b100;
+  s_inm = 0; 
+
+  // Ciclo 12 (skpine R5, R4)
+  #20 
+  s_inc = 1;
+  s_skip = 0;
+  we3 = 1;
+  Op = 3'b011;
+  s_inm = 0;
+
+  // Ciclo 13 (add R1, R2, R1)
+  #20 
+  s_inc = 1;
+  s_skip = 0;
+  we3 = 1;
+  Op = 3'b010;
+  s_inm = 0;
+
+  // Ciclo 14 (sub R3, R2, R3)
+  #20 
+  s_inc = 1;
+  s_skip = 0;
+  we3 = 1;
+  Op = 3'b011;
+  s_inm = 0;
+
+  // Ciclo 15 (skipeq R3, R2)
+  #20 
+  s_inc = 1;
+  s_skip = 0;
+  we3 = 0;
+  Op = 3'b011;
+  s_inm = 0;
+
+  // Ciclo 16 (j Bucle)
+  #20 
+  s_inc = 0;
+  s_skip = 0;
+  we3 = 0;
+  Op = 3'b000;
+  s_inm = 0;
+
+  // Ciclo 17 (and R3, R2, R5)(Bucle: )
+  #20 
+  s_inc = 1;
+  s_skip = 0;
+  we3 = 1;
+  Op = 3'b100;
+  s_inm = 0;
+
+  // Ciclo 18 (skipne R5, R4)
+  #20  
+  s_inc = 1;
+  s_skip = 0;
+  we3 = 0;
+  Op = 3'b011;
+  s_inm = 0;
+
+  // Ciclo 19 (add R1, R2, R1)
+  #20 
+  s_inc = 1;
+  s_skip = 0;
+  we3 = 1;
+  Op = 3'b010;
+  s_inm = 0;
+
+  // Ciclo 20 (sub R3, R2, R3)
+  #20 
+  s_inc = 1;
+  s_skip = 0;
+  we3 = 1;
+  Op = 3'b011;
+  s_inm = 0;
+
+  // Ciclo 21 (skipeq R3, R2)
+  #20 
+  s_inc = 1;
+  s_skip = 1;
+  we3 = 0;
+  Op = 3'b011;
+  s_inm = 0;
+
+  // Ciclo 22 (j Fin)
+  #20 
+  s_inc = 0;
+  s_skip = 0;
+  we3 = 0;
+  Op = 3'b000;
+  s_inm = 0;
+
+  // Ciclo 23 (j Fin)
+  #20 
+  s_inc = 0;
+  s_skip = 0;
+  we3 = 0;
+  Op = 3'b000;
+  s_inm = 0;;
+
   $finish;
 end
 
